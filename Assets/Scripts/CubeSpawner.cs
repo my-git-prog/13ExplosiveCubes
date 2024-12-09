31803b69f91ad5e93f1b1d661dfb5873509b679a
my-git-prog <pavel.serpukhov@gmail.com>
2024-12-09 21:25:07 +0300

    add cube explosion

--- Assets/Scripts/CubeSpawner.cs
+++ Assets/Scripts/CubeSpawner.cs
@@ -8,29 +8,38 @@ public class CubeSpawner : MonoBehaviour
     [SerializeField] private int _startCubesCount = 3;
     [SerializeField] private int _minSubCubesCount = 2;
     [SerializeField] private int _maxSubCubesCount = 6;
+    [SerializeField] private float _startSplitChance = 1f;
+    [SerializeField] private float _startExplosionRadius = 2.5f;
+    [SerializeField] private float _startExplosionForce = 150f;
 
     private List<ExplosiveCube> _explosiveCubes = new List<ExplosiveCube>();
 
     private void Start()
     {
-        CreateNewCubes(_startCubesCount, _startSpawnPosition, _explosiveCubePrefab.transform.localScale);
+        CreateNewCubes(_startCubesCount, _startSpawnPosition, _explosiveCubePrefab.transform.localScale,
+            _startSplitChance, _startExplosionRadius, _startExplosionForce);
     }
 
     private void OnDisable()
     {
         foreach(ExplosiveCube cube in _explosiveCubes)
         {
-            cube.Clicked -= CreateSubCubes;
+            cube.Clicked -= DestroyClickedCube;
         }
     }
 
-    private void CreateSubCubes(ExplosiveCube cube)
+    private void DestroyClickedCube(ExplosiveCube cube)
     {
         if(Random.value <= cube.SplitChance)
         {
             int count = Random.Range(_minSubCubesCount, _maxSubCubesCount + 1);
 
-            CreateNewCubes(count, cube.transform.position, cube.transform.localScale / 2, cube.SplitChance / 2);
+            CreateNewCubes(count, cube.transform.position, cube.transform.localScale / 2, cube.SplitChance / 2,
+                cube.ExplosionRadius * 2, cube.ExplosionForce * 2);
+        }
+        else
+        {
+            cube.Explode();
         }
 
         RemoveCube(cube);
@@ -39,18 +48,19 @@ public class CubeSpawner : MonoBehaviour
     private void RemoveCube(ExplosiveCube cube)
     {
         _explosiveCubes.Remove(cube);
-        cube.Clicked -= CreateSubCubes;
+        cube.Clicked -= DestroyClickedCube;
         cube.Destroy();
     }
 
-    private void CreateNewCubes(int count, Vector3 position, Vector3 scale, float splitChance = 1f)
+    private void CreateNewCubes(int count, Vector3 position, Vector3 scale, float splitChance, 
+                                float explosionRadius, float explosionForce)
     {
         for (int i = 0; i < count; i++)
         {
             ExplosiveCube newCube = Instantiate(_explosiveCubePrefab, position, Random.rotation);
 
-            newCube.Initialize(scale, Random.onUnitSphere, splitChance);
-            newCube.Clicked += CreateSubCubes;
+            newCube.Initialize(scale, Random.onUnitSphere, splitChance, explosionRadius, explosionForce);
+            newCube.Clicked += DestroyClickedCube;
             _explosiveCubes.Add(newCube);
         }
     }
--- Assets/Scripts/ExplosiveCube.cs
+++ Assets/Scripts/ExplosiveCube.cs
@@ -1,31 +1,60 @@
+using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.Events;
 
 [RequireComponent(typeof(Rigidbody))]
 public class ExplosiveCube : MonoBehaviour
 {
-    [SerializeField] private float _afterSplitMoveForce;
     [SerializeField] private Rigidbody _rigidbody;
 
     private float _splitChance = 1f;
+    private float _explosionRadius;
+    private float _explosionForce;
     public event UnityAction<ExplosiveCube> Clicked;
 
     public float SplitChance => _splitChance;
+    public float ExplosionRadius => _explosionRadius;
+    public float ExplosionForce => _explosionForce;
 
     private void OnMouseUpAsButton()
     {
         Clicked.Invoke(this);
     }
 
-    public void Initialize(Vector3 scale, Vector3 forceDirection, float splitChance)
+    public void Initialize(Vector3 scale, Vector3 forceDirection, float splitChance, float explosionRadius, float explosionForce)
     {
         transform.localScale = scale;
-        _rigidbody.AddForce(forceDirection * _afterSplitMoveForce);
         _splitChance = splitChance;
+        _explosionRadius = explosionRadius;
+        _explosionForce = explosionForce;
+        _rigidbody.AddForce(forceDirection * _explosionForce);
     }
 
     public void Destroy()
     {
         Destroy(gameObject);
+    }
+
+    public void Explode()
+    {
+        foreach(Rigidbody explodableObject in GetExplodableObjects())
+        {
+            explodableObject.AddExplosionForce(_explosionForce, transform.position, _explosionRadius);
+        }
+    }
+
+    private List<Rigidbody> GetExplodableObjects()
+    {
+        Collider[] hits = Physics.OverlapSphere(transform.position, _explosionRadius);
+
+        List<Rigidbody> cubes = new();
+
+        foreach (Collider hit in hits)
+        {
+            if(hit.attachedRigidbody != null)
+                cubes.Add(hit.attachedRigidbody);
+        }
+
+        return cubes;
     }
 }
